1b2e7d5b1d291a45abde5596be78cef47a40e998
kh45 <khaledhassan45@gmail.com>
2019-10-29 16:40:54 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById('create-task-form');
+  form.addEventListener("submit", handleForm)
 });
+
+const list = document.getElementById('tasks');
+
+function handleForm(event){
+  event.preventDefault();
+  let newTask = document.querySelector("#new-task-description").value;
+  addTask(newTask);
+  event.target.reset()
+}
+
+function addTask(newTask){
+  let liElem = document.createElement("li");
+  list.appendChild(liElem);
+
+  liElem.innerText = newTask
+}
